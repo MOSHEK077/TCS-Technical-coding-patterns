681edc00b7b0561b1600e1265a36f95bda35b886
jonesmoshekofficial@gmail.com <jonesmoshekofficial@gmail.com>
2026-03-10 16:02:21 +0530

    leap year problem added

---
+++ p2.java
@@ -0,0 +1,16 @@
+class p2{
+    public static void main(String[] args) {
+        if(args.length==0){
+            System.out.println("Provide a year in the command line");
+            return;
+        }
+
+        int year = Integer.parseInt(args[0]);
+        if((year % 4 == 0 && year % 100 != 0 )||year % 400 == 0){
+            System.out.println(year+" is a leap year...............");
+        }
+        else{
+            System.out.println(year+" is not a leap year...............");
+        }
+    }
+}
